910fcefb5cc0e58bf72706cdedaf5a1167d42359
Andrew Lisowski <Andrew_Lisowski@intuit.com>
2018-05-01 18:36:17 -0700

    get files, read, parse markdown and write to dst folder as html

--- src/ignite.js
+++ src/ignite.js
@@ -1,10 +1,29 @@
-import fs from 'fs';
 import 'babel-polyfill';
+
+import fs from 'fs';
+import path from 'path';
 import { promisify } from 'util';
+import Remarkable from 'remarkable';
 
+const mkDir = promisify(fs.mkdir);
 const readDir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
-export default async function build({ src = 'docs/' }) {
+const markdownRenderer = new Remarkable();
+
+export default async function build({ src = 'docs/', dst = '_ignite/' }) {
   const docs = await readDir(src);
-  console.log(docs);
+
+  if (!fs.existsSync(dst)) {
+    await mkDir(dst);
+  }
+
+  docs.map(async file => {
+    const markdown = await readFile(path.join(src, file), 'utf8');
+    const html = markdownRenderer.render(markdown);
+    const base = path.basename(file, '.md');
+
+    await writeFile(path.join(dst, `${base}.html`), html);
+  });
 }
