5d8e831563b47912facb7cd357e6c268b10e37e2
Andrew Lisowski <Andrew_Lisowski@intuit.com>
2018-05-01 19:05:52 -0700

    add nice logging

--- src/watch.js
+++ src/watch.js
@@ -1,25 +1,34 @@
+import log from 'log-update';
 import serve from 'serve';
 import watch from 'watch';
 import build from './ignite';
 
+const PORT = 8008;
 let server;
 
 function startServer(dist) {
   server = serve(dist, {
-    port: 1337,
+    silent: true,
+    port: PORT,
     ignore: ['node_modules']
   });
 }
 
 export default async function watchDocs(argv) {
-  const { src, dist } = argv;
+  const { src, dst } = argv;
 
-  startServer(dist);
+  startServer(dst);
 
   watch.watchTree(src, async () => {
-    console.log('Building docs...');
+    log('Building docs...');
     server.stop();
     await build(argv);
-    startServer();
+    log(`
+      Serving docs at:
+
+        Local:   http://localhost:${PORT}
+        Network: http://192.168.1.14:${PORT}
+    `);
+    startServer(dst);
   });
 }
