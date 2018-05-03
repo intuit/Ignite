f63b83014bcdc4066782d0e878222f7e8b0c0f55
Andrew Lisowski <Andrew_Lisowski@intuit.com>
2018-05-02 19:22:06 -0700

    should only need to webpack once

--- webpack.withDocs.config.js
+++ webpack.withDocs.config.js
@@ -4,54 +4,58 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MarkdownRenderer = require('marked').Renderer;
 const highlightjs = require('highlight.js');
 
-const renderer = new MarkdownRenderer();
-renderer.code = (code, language) => {
-  const validLang = Boolean(language && highlightjs.getLanguage(language));
-  const highlighted = validLang
-    ? highlightjs.highlight(language, code).value
-    : code;
-  return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
-};
+// const renderer = new MarkdownRenderer();
+// renderer.code = (code, language) => {
+//   const validLang = Boolean(language && highlightjs.getLanguage(language));
+//   const highlighted = validLang
+//     ? highlightjs.highlight(language, code).value
+//     : code;
+//   return `<pre><code class="hljs ${language}">${highlighted}</code></pre>`;
+// };
 
 module.exports = function(options = {}) {
-  const docs = globby.sync([path.join(options.src, '**/*.md')]);
-  const docHTML = docs.map(doc => {
-    return new HtmlWebPackPlugin({
-      template: doc,
-      filename: doc.replace('.md', '.html')
-    });
-  });
+  // const docs = globby.sync([path.join(options.src, '**/*.md')]);
+  // const docHTML = docs.map(doc => {
+  //   return new HtmlWebPackPlugin({
+  //     template: doc,
+  //     filename: doc.replace('.md', '.html')
+  //   });
+  // });
 
   return {
-    entry: [...docs.map(doc => path.resolve(doc)), './src/index.js'],
+    mode: 'development',
+    entry: [
+      // ...docs.map(doc => path.resolve(doc)),
+      './src/index.js'
+    ],
 
     output: {
-      path: path.resolve(options.dst),
+      // path: options.dst ? path.resolve(options.dst) : null,
       filename: 'bundle.js'
     },
 
-    devServer: {
-      contentBase: path.join(__dirname, 'dist')
-    },
+    // devServer: {
+    //   contentBase: path.join(__dirname, 'dist')
+    // },
 
     module: {
       rules: [
-        {
-          test: /\.md$/,
-          use: [
-            {
-              loader: 'html-loader'
-            },
-            // Create loader to transform .md links to .html
-            {
-              loader: 'markdown-loader',
-              options: {
-                pedantic: true,
-                renderer
-              }
-            }
-          ]
-        },
+        // {
+        //   test: /\.md$/,
+        //   use: [
+        //     {
+        //       loader: 'html-loader'
+        //     },
+        //     // Create loader to transform .md links to .html
+        //     {
+        //       loader: 'markdown-loader',
+        //       options: {
+        //         pedantic: true,
+        //         renderer
+        //       }
+        //     }
+        //   ]
+        // },
         {
           test: /\.js$/,
           exclude: /node_modules/,
@@ -92,7 +96,7 @@ module.exports = function(options = {}) {
     },
 
     plugins: [
-      ...docHTML,
+      // ...docHTML,
       new HtmlWebPackPlugin({
         template: './src/index.html',
         filename: './index.html'
