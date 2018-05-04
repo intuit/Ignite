192688e02ce60263c618ee801ed6dd386f1d8c4f
Andrew Lisowski <Andrew_Lisowski@intuit.com>
2018-05-04 01:03:00 -0700

    use innerhtml to get it all!

--- __tests__/html-to-react.js
+++ __tests__/html-to-react.js
@@ -29,4 +29,12 @@ test('sanitizeJSX', () => {
     </code>
 `)
   ).toMatchSnapshot();
+
+  expect(
+    sanitizeJSX(`
+    {
+      some: 'object
+    }
+  `)
+  ).toBe();
 });
--- src/html-to-react.js
+++ src/html-to-react.js
@@ -7,24 +7,7 @@ export function insertInString(a, b, position) {
 }
 
 export function sanitizeJSX(source) {
-  let codeTag = source.indexOf('<code');
-
-  if (codeTag === -1) {
-    return source;
-  }
-
-  // ` character will will break the wrapping below
   source = source.replace(new RegExp('`', 'g'), "'");
-
-  while (codeTag !== -1) {
-    const endCodeBracket = source.indexOf('>', codeTag);
-    source = insertInString(source, '{`', endCodeBracket + 1);
-
-    const endCodeTag = source.indexOf('</code>', endCodeBracket);
-    source = insertInString(source, '`}', endCodeTag - 1);
-
-    codeTag = source.indexOf('<code', endCodeTag);
-  }
 
   return source;
 }
@@ -40,9 +23,9 @@ export default function(source) {
     import ignite from 'ignite';
 
     function markDownPage() {
-      return <div>
-        ${sanitizeJSX(unescape(source))}
-      </div>
+      return <div dangerouslySetInnerHTML={{  __html: \`${sanitizeJSX(
+        source
+      )}\`  }} />
     };
     
     export default ignite('${pathToMarkdown}', markDownPage, ${isIndex});
