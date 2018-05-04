787ff6f065394dc79561558fc8d0d613892a2966
Andrew Lisowski <Andrew_Lisowski@intuit.com>
2018-05-03 23:03:25 -0700

    need to wrap

--- example/moreDocs/example.md
+++ example/moreDocs/example.md
@@ -12,3 +12,13 @@ export default function({
     path       // An array representing the path of this asset in the view
 }){}
 ```
+
+
+```javascript
+export default function({
+    value,     // An asset to transform without the 'asset' wrapper
+    fuego,     // The fuegojs instance to get your state from 
+    player,    // An instance of fuego-player making the transform request
+    path       // An array representing the path of this asset in the view
+}){}
+```
--- src/html-to-react.js
+++ src/html-to-react.js
@@ -1,18 +1,44 @@
 import path from 'path';
 import { getOptions } from 'loader-utils';
 
+function insertInString(a, b, position) {
+  return a.substr(0, position) + b + a.substr(position);
+}
+
+function injectPreTemplate(source) {
+  let codeTag = source.indexOf('<code')
+
+  if (codeTag === -1) {
+    return source;
+  }
+
+  let count = 4
+  while(count--) {
+    const endCodeBracket = source.indexOf('>', codeTag)
+    source = insertInString(source, '{`', endCodeBracket + 1)
+  
+    const endCodeTag = source.indexOf('</code>')
+    source = insertInString(source, '`}', endCodeTag - 1)
+    console.log(source)
+
+    codeTag = source.indexOf('<code', endCodeTag)
+  }
+
+
+  return source;
+}
 
 export default function(source) {
   const options = getOptions(this);
   const pathToMarkdown = path.relative(options.src, this.resourcePath);
   const isIndex = this.resourcePath.includes(path.join(options.src, options.index));
-  console.log(source)
+
   return `
     import ignite from 'ignite';
 
-    function htmlComponent() {
+    function markDownPage() {
       return <div>
-        ${escapeJSX(source)}
+        ${injectPreTemplate(source)}
       </div>
     };
     
