ab9b2144a791e4d8b3c22294259182402653017c
Andrew Lisowski <Andrew_Lisowski@intuit.com>
2018-05-08 01:46:58 -0700

    switch to bulma footer

--- src/app/components/Footer/index.js
+++ src/app/components/Footer/index.js
@@ -5,9 +5,15 @@ import styles from './footer.css';
 
 const Footer = () => (
   <footer className={makeClass(styles.footer, 'footer')}>
-    <p className={makeClass('has-text-centered', styles.copyright)}>
-      Made with 🌳🔥💨 by Intuit. Licensed under the MIT License.
-    </p>
+    <div className="container">
+      <div className={makeClass('has-text-centered', 'content')}>
+        <p className={styles.copyright}>
+          Made with 🌳🔥💨 by Intuit. Licensed under the{' '}
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>{' '}
+          License.
+        </p>
+      </div>
+    </div>
   </footer>
 );
 
