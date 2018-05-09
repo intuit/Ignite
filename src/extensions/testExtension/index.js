f087c2249823a948c446a4f350687ab556d9d7b7
Andrew Lisowski <Andrew_Lisowski@intuit.com>
2018-05-08 18:57:13 -0700

    test if state works

--- src/extensions/testExtension/index.js
+++ src/extensions/testExtension/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import makeClass from 'classnames';
 import PropTypes from 'prop-types';
-import makePlugin from '../ignite-plugin';
 
 import styles from './test.css';
 
-const TestExtension = props => {
-  return (
-    <div>
-      <h1 className={styles.header}> What a useful plugin I am! </h1>
-      {props.children}
-    </div>
-  );
-};
+class TestExtension extends Component {
+  state = {
+    count: 0
+  };
+
+  render() {
+    const { count } = this.state;
+    const { children } = this.props;
+
+    return (
+      <div>
+        <h1 className={styles.header}> What a useful plugin I am! </h1>
+        {children}
+
+        <h2>Count</h2>
+        <p>{this.state.count}</p>
+        <button
+          className="button is-success"
+          type="button"
+          onClick={() => this.setState({ count: count - 1 })}
+        >
+          -
+        </button>
+        <button
+          className={makeClass('button', 'is-success', styles.addButton)}
+          type="button"
+          onClick={() => this.setState({ count: count + 1 })}
+        >
+          +
+        </button>
+      </div>
+    );
+  }
+}
 
 TestExtension.propTypes = {
   children: PropTypes.node
