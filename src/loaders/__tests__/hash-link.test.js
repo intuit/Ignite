4c1e939da25be05a3e525f2aff282270324f1d96
Andrew Lisowski <Andrew_Lisowski@intuit.com>
2018-05-10 15:59:20 -0700

    fix tests

--- src/loaders/__tests__/hash-link.test.js
+++ src/loaders/__tests__/hash-link.test.js
@@ -1,42 +1,31 @@
-import haskLink from '../hash-link';
+import { transformLink } from '../hash-link';
 
-describe('haskLink', () => {
+describe('transformLink', () => {
   test('should leave http links alone', () => {
     expect(
-      haskLink(`
-      [Markdown link 1](https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350)
-      [Markdown link 2](https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350)
-    `)
-    ).toBe(`
-      [Markdown link 1](https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350)
-      [Markdown link 2](https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350)
-    `);
+      transformLink(
+        '/path/',
+        'https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350'
+      )
+    ).toBe(
+      'https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350'
+    );
   });
 
   test('should hasg link', () => {
     expect(
-      haskLink(`
-      [Markdown link 1](./markdown.md)
-      [Markdown link 2](./markdown2.md)
-    `)
-    ).toBe(`
-      [Markdown link 1](#/markdown.md)
-      [Markdown link 2](#/markdown2.md)
-    `);
+      transformLink('/docs/example.md', './markdown.md', { src: 'docs/' })
+    ).toBe('../../../../docs/markdown.md');
   });
 
   test('should make relative paths to images', () => {
     expect(
-      haskLink.bind({ resourcePath: '/path/' })(
-        `
-      [Image 1](./link/to/image.png)
-      [Image 2](./link/to/another.png "With a description")
-    `,
-        '/some/path'
-      )
-    ).toBe(`
-      [Image 1](../../link/to/image.png )
-      [Image 2](../../link/to/another.png "With a description")
-    `);
+      transformLink('/path/', './link/to/image.png', { src: 'docs/' })
+    ).toBe('../../../../link/to/image.png ');
+    expect(
+      transformLink('/path/', './link/to/image.png "With a description"', {
+        src: 'docs/'
+      })
+    ).toBe('../../../../link/to/image.png "With a description"');
   });
 });
--- src/loaders/hash-link.js
+++ src/loaders/hash-link.js
@@ -2,28 +2,32 @@ import path from 'path';
 import transformLinks from 'transform-markdown-links';
 import { getOptions } from 'loader-utils';
 
+export function transformLink(resourcePath, link, options) {
+  if (link.includes('http')) {
+    return link;
+  }
+
+  const pathToThisSource = path.dirname(path.resolve(resourcePath));
+
+  if (path.extname(link).includes('.md')) {
+    const pathToLink = path.join(pathToThisSource, link);
+    const pathToDocs = path.join(process.cwd(), options.src);
+    const correctPath = path.relative(pathToDocs, pathToLink);
+    return path.join('#', correctPath);
+  }
+
+  const [filePath, ...description] = link.split(' ');
+  const pathToLink = path.join(pathToThisSource, filePath);
+  const correctPath = path.relative(process.cwd(), pathToLink);
+
+  return `${correctPath} ${description.join(' ')}`;
+}
+
 export default function(source) {
   const options = getOptions(this);
-  const markdown = transformLinks(source, link => {
-    if (link.includes('http')) {
-      return link;
-    }
-
-    const pathToThisSource = path.dirname(path.resolve(this.resourcePath));
-
-    if (path.extname(link).includes('.md')) {
-      const pathToLink = path.join(pathToThisSource, link);
-      const pathToDocs = path.join(process.cwd(), options.src);
-      const correctPath = path.relative(pathToDocs, pathToLink);
-      return path.join('#', correctPath);
-    }
-
-    const [filePath, ...description] = link.split(' ');
-    const pathToLink = path.join(pathToThisSource, filePath);
-    const correctPath = path.relative(process.cwd(), pathToLink);
-
-    return `${correctPath} ${description.join(' ')}`;
-  });
+  const markdown = transformLinks(source, link =>
+    transformLink(this.resourcePath, link, options)
+  );
 
   return markdown;
 }
