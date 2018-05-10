c7b4d1af4343fb9af914c088abb77b522699d4a7
Andrew Lisowski <Andrew_Lisowski@intuit.com>
2018-05-10 13:52:22 -0700

    ci - publish that works.... hopefully

---
+++ scripts/ci-publish.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+echo 'CI build - Publishing'
+npm version `github-version` -m '%s [skip ci]'
+
+PACKAGE_VERSION=$(cat package.json \
+| grep version \
+| head -1 \
+| awk -F: '{ print $2 }' \
+| sed 's/[",]//g')
+
+echo 'CI build - Generating CHANGELOG'
+echo "\n$PACKAGE_VERSION\n" >> CHANGELOG.md
+github-release -d >> CHANGELOG.md
+git add CHANGELOG.md
+git commit -m 'Adding CHANGELOG.md [skip ci]'
+
+npm publish
+git push --follow-tags --set-upstream origin $branch
+./github-release
