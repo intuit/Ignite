d4906367abeb8b6222163a60df56f21d9fb534e4
Andrew Lisowski <Andrew_Lisowski@intuit.com>
2018-05-16 10:45:57 -0700

    remove old test file

--- src/app/components/__tests__/footer.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import Footer from '../Footer';
-import renderToJson from './utils/render-to-json';
-
-test('Footer', () => {
-  expect(renderToJson(<Footer />)).toMatchSnapshot();
-});
