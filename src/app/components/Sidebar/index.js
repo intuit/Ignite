a61edf33762adc99fb6a458e81b53e6cbce9098e
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-05-17 01:54:12 -0700

    lint

--- src/app/components/Sidebar/index.js
+++ src/app/components/Sidebar/index.js
@@ -5,6 +5,7 @@ import makeClass from 'classnames';
 
 import Icon from '../Icon';
 import styles from './sidebar.css';
+
 export class Sidebar extends Component {
   static propTypes = {
     className: PropTypes.string,
