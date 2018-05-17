29c86bfaa91f8aa0577744faa29f1e3f81b648f0
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-05-17 14:49:23 -0700

    if sidebar has no content it displays nothing

--- src/app/components/Sidebar/index.js
+++ src/app/components/Sidebar/index.js
@@ -38,6 +38,10 @@ export class Sidebar extends Component {
   };
 
   render() {
+    if (!this.props.content) {
+      return null;
+    }
+
     return (
       <div
         className={makeClass(
