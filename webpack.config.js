38b4589a7a5624d07bd465c9115cbf637540f991
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-06-06 12:49:22 -0700

    lint

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = function(options = {}) {
     output: {
       path: dest,
       filename: 'bundle.js'
-      // publicPath: './'
+      // PublicPath: './'
     },
 
     module: {
