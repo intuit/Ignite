9e768230afca9ce0ef25f1ec9849aff7a039f469
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-06-07 13:52:44 -0700

    add applescript to use same tab in chrome when testing

--- src/ignite.js
+++ src/ignite.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 import fs from 'fs';
+import { execSync } from 'child_process';
 import path from 'path';
+
 import git from 'simple-git/promise';
 import dayjs from 'dayjs';
 import globby from 'globby';
@@ -12,6 +14,7 @@ import webpack from 'webpack';
 import serve from 'webpack-serve';
 import ghpages from 'gh-pages';
 import createStaticWebsite from 'react-snap';
+import webpackServeWaitpage from 'webpack-serve-waitpage';
 
 import config from '../webpack.config';
 import packageJSON from '../package';
@@ -191,7 +194,29 @@ export default async function build(options) {
       open: true,
       logLevel: 'silent',
       dev: { logLevel: 'silent' },
-      hot: { logLevel: 'silent' }
+      hot: { logLevel: 'silent' },
+      add: (app, middleware, options) => {
+        app.use(
+          webpackServeWaitpage(options, {
+            title: 'Ignite Dev Server',
+            theme: 'material'
+          })
+        );
+      },
+      on: {
+        listening: () => {
+          execSync('ps cax | grep "Google Chrome"');
+          execSync(
+            `osascript ../src/chrome.applescript "${encodeURI(
+              `http://localhost:${options.port}`
+            )}"`,
+            {
+              cwd: __dirname,
+              stdio: 'ignore'
+            }
+          );
+        }
+      }
     });
   } else {
     const compiler = webpack(webpackConfig);
