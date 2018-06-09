b0d23629e592eaa9cea0291f06d523256289bfc0
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-06-08 18:20:19 -0700

    add js uglifier and html minifier

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const path = require('path');
 const webpack = require('webpack');
 const globby = require('globby');
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -27,6 +28,7 @@ module.exports = function(options) {
   const logoPath = options.logo ? path.join(options.src, options.logo) : null;
   const logoExists = logoPath && fs.existsSync(path.resolve(logoPath));
   const dest = options.dst ? path.resolve(options.dst) : null;
+  const isProd = options.mode === 'production';
 
   return {
     mode: options.mode,
@@ -37,7 +39,59 @@ module.exports = function(options) {
       path.resolve(__dirname, './src/app/index.js')
     ].filter(Boolean),
 
-    devtool: 'source-map',
+    devtool: isProd ? false : 'source-map',
+
+    optimization: {
+      minimizer: [
+        isProd &&
+          new UglifyJsPlugin({
+            uglifyOptions: {
+              parse: {
+                // we want uglify-js to parse ecma 8 code. However, we don't want it
+                // to apply any minfication steps that turns valid ecma 5 code
+                // into invalid ecma 5 code. This is why the 'compress' and 'output'
+                // sections only apply transformations that are ecma 5 safe
+                // https://github.com/facebook/create-react-app/pull/4234
+                ecma: 8
+              },
+              compress: {
+                ecma: 5,
+                warnings: false,
+                // Disabled because of an issue with Uglify breaking seemingly valid code:
+                // https://github.com/facebook/create-react-app/issues/2376
+                // Pending further investigation:
+                // https://github.com/mishoo/UglifyJS2/issues/2011
+                comparisons: false
+              },
+              mangle: {
+                safari10: true
+              },
+              output: {
+                ecma: 5,
+                comments: false,
+                // Turned on because emoji and regex is not minified properly using default
+                // https://github.com/facebook/create-react-app/issues/2488
+                ascii_only: true
+              }
+            },
+            // Use multi-process parallel running to improve the build speed
+            // Default number of concurrent runs: os.cpus().length - 1
+            parallel: true,
+            // Enable file caching
+            cache: true
+          })
+      ].filter(Boolean),
+      // Automatically split vendor and commons
+      // https://twitter.com/wSokra/status/969633336732905474
+      // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+      splitChunks: {
+        chunks: 'all',
+        name: 'vendors'
+      },
+      // Keep the runtime chunk seperated to enable long term caching
+      // https://twitter.com/wSokra/status/969679223278505985
+      runtimeChunk: true
+    },
 
     devServer: {
       quiet: true,
@@ -162,7 +216,21 @@ module.exports = function(options) {
         codeStyle: options.codeStyle,
         bulmaTheme: options.bulmaTheme,
         template: path.resolve(__dirname, './src/index.html'),
-        filename: './index.html'
+        filename: './index.html',
+        minify: isProd
+          ? {
+              removeComments: true,
+              collapseWhitespace: true,
+              removeRedundantAttributes: true,
+              useShortDoctype: true,
+              removeEmptyAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              keepClosingSlash: true,
+              minifyJS: true,
+              minifyCSS: true,
+              minifyURLs: true
+            }
+          : null
       }),
       new HtmlWebPackPlugin({
         baseURL: options.baseURL,
@@ -182,7 +250,7 @@ module.exports = function(options) {
         }
       }),
       new FriendlyErrorsWebpackPlugin({
-        clearConsole: options.mode === 'development',
+        clearConsole: !isProd,
         compilationSuccessInfo: options.compilationSuccessInfo
       })
     ].filter(Boolean)
