3bc0142b020c0d8ac4317e91444ad6f524204680
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-06-12 18:34:37 -0700

    route plugins through plugin provider to get lazy loading

--- src/extensions/jsx-inline/index.js
+++ src/extensions/jsx-inline/index.js
@@ -1,5 +1,23 @@
 /* eslint-disable camelcase */
 
+function injectPluginProvider(markup) {
+  const componentRegex = /<([A-Z][A-Za-z]+)/;
+  const sources = [];
+
+  let match = componentRegex.exec(markup);
+
+  while (match) {
+    console.log(match[1]);
+    markup = markup.replace(
+      componentRegex,
+      "<pluginprovider name=!{'$1'!} plugins=!{props.plugins!}"
+    );
+    match = componentRegex.exec(markup);
+  }
+
+  return markup;
+}
+
 export default function jsxInline(md) {
   md.renderer.rules.jsx_inline = function(tokens, idx) {
     let { content } = tokens[idx];
@@ -7,6 +25,8 @@ export default function jsxInline(md) {
     if (content[0] !== '{') {
       content = content.replace(new RegExp('{', 'g'), '!{');
       content = content.replace(new RegExp('}', 'g'), '!}');
+
+      content = injectPluginProvider(content);
     }
 
     return content;
