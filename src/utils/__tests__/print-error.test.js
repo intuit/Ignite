988c14e9dc6f0ccb2924e99cc54e3de29fce5a44
Andrew Lisowski <lisowski54@gmail.com>
2018-07-11 16:04:27 -0700

    test more things

---
+++ src/utils/__tests__/print-error.test.js
@@ -0,0 +1,21 @@
+import printError from '../print-error';
+
+describe('printError', () => {
+  test('string', () => {
+    const mock = jest.fn();
+    console.error = mock;
+
+    printError('An error String');
+
+    expect(mock).toMatchSnapshot();
+  });
+
+  test('error', () => {
+    const mock = jest.fn();
+    console.error = mock;
+
+    printError(new Error('An error String'));
+
+    expect(mock).toMatchSnapshot();
+  });
+});
